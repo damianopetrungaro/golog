a185f021dd2ef9906fce14b43eeb8b89c90c69a1
Damiano Petrungaro <damiano@pento.io>
2022-04-23 18:57:00 +0200

    chore: add benchmarks for the encoder

---
+++ benchmarks/encoder/json.go
@@ -0,0 +1,221 @@
+package encoder
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"strconv"
+	"time"
+
+	"github.com/damianopetrungaro/golog"
+	gojson "github.com/goccy/go-json"
+	jsoniter "github.com/json-iterator/go"
+)
+
+type JsonEncoder struct{}
+
+func (j *JsonEncoder) StdLinEncode(e golog.Entry) (io.WriterTo, error) {
+	log := map[string]any{
+		"level":     e.Level().String(),
+		"timestamp": time.Now().Format(`2006-01-02`),
+		"message":   e.Message(),
+	}
+
+	j.mapEncodeFields(e.Fields(), log)
+
+	w := &bytes.Buffer{}
+	if err := json.NewEncoder(w).Encode(log); err != nil {
+		return nil, err
+	}
+
+	w.WriteString(`\n`)
+	return w, nil
+}
+
+func (j *JsonEncoder) GoJsonEncode(e golog.Entry) (io.WriterTo, error) {
+	log := map[string]any{
+		"level":     e.Level().String(),
+		"timestamp": time.Now().Format(`2006-01-02`),
+		"message":   e.Message(),
+	}
+	j.mapEncodeFields(e.Fields(), log)
+
+	w := &bytes.Buffer{}
+	if err := gojson.NewEncoder(w).Encode(log); err != nil {
+		return nil, err
+	}
+
+	w.WriteString(`\n`)
+	return w, nil
+}
+
+func (j *JsonEncoder) JsoniterEncode(e golog.Entry) (io.WriterTo, error) {
+	log := map[string]any{
+		"level":     e.Level().String(),
+		"timestamp": time.Now().Format(`2006-01-02`),
+		"message":   e.Message(),
+	}
+	j.mapEncodeFields(e.Fields(), log)
+
+	raw, err := jsoniter.Marshal(log)
+	if err != nil {
+		return nil, err
+	}
+
+	w := &bytes.Buffer{}
+	w.Write(raw)
+	w.WriteString(`\n`)
+	return w, nil
+}
+
+func (j *JsonEncoder) mapEncodeFields(flds golog.Fields, log map[string]any) {
+	if len(flds) == 0 {
+		return
+	}
+	fields := map[string]any{}
+	for _, f := range flds {
+		fields[f.Key()] = f.Key()
+	}
+	log["fields"] = fields
+}
+
+func (j *JsonEncoder) ManualEncode(e golog.Entry) (io.WriterTo, error) {
+	w := &bytes.Buffer{}
+	w.WriteString(`{`)
+	addElemQuoted(w, "level", e.Level().String())
+	w.WriteString(`,`)
+	addElemQuoted(w, "timestamp", time.Now().Format(`2006-01-02`))
+	w.WriteString(`,`)
+	addElemQuoted(w, "message", e.Message())
+	j.manualEncodeFields(e.Fields(), w)
+	w.WriteString(`}\n`)
+	return w, nil
+}
+
+func (j *JsonEncoder) manualEncodeFields(flds golog.Fields, w *bytes.Buffer) {
+	if len(flds) == 0 {
+		return
+	}
+
+	w.WriteString(`,"fields":{`)
+	for i, f := range flds {
+		j.encodeField(f, w)
+		if i != len(flds)-1 {
+			w.WriteString(`,`)
+		}
+	}
+	w.WriteString(`}`)
+}
+
+func (j *JsonEncoder) encodeField(f golog.Field, w *bytes.Buffer) {
+	switch t := f.Value().(type) {
+	case bool:
+		addElem(w, f.Key(), strconv.FormatBool(t))
+	case []bool:
+		addElements(w, f.Key(), func(w *bytes.Buffer) {
+			for i, val := range t {
+				addArrayElem(w, strconv.FormatBool(val), i != len(t)-1)
+			}
+		})
+	case string:
+		addElemQuoted(w, f.Key(), t)
+	case error:
+		switch t {
+		case nil:
+			addElemQuoted(w, f.Key(), "null")
+		default:
+			addElemQuoted(w, f.Key(), t.Error())
+		}
+	case []string:
+		addElements(w, f.Key(), func(w *bytes.Buffer) {
+			for i, val := range t {
+				addArrayElemQuoted(w, val, i != len(t)-1)
+			}
+		})
+	case []error:
+		addElements(w, f.Key(), func(w *bytes.Buffer) {
+			for i, val := range t {
+				switch t {
+				case nil:
+					addElemQuoted(w, f.Key(), "null")
+				default:
+					addElemQuoted(w, f.Key(), val.Error())
+				}
+				if i != len(t)-1 {
+					w.WriteString(`,`)
+				}
+			}
+		})
+	case uint:
+		addElem(w, f.Key(), strconv.Itoa(int(t)))
+	case int:
+		addElem(w, f.Key(), strconv.Itoa(t))
+	case []uint:
+		addElements(w, f.Key(), func(w *bytes.Buffer) {
+			for i, val := range t {
+				addArrayElem(w, strconv.Itoa(int(val)), i != len(t)-1)
+			}
+		})
+	case []int:
+		addElements(w, f.Key(), func(w *bytes.Buffer) {
+			for i, val := range t {
+				addArrayElem(w, strconv.Itoa(val), i != len(t)-1)
+			}
+		})
+	case float64:
+		addElem(w, f.Key(), strconv.FormatFloat(t, 'f', 10, 64))
+	case float32:
+		addElem(w, f.Key(), strconv.FormatFloat(float64(t), 'f', 10, 32))
+	case []float64:
+		addElements(w, f.Key(), func(w *bytes.Buffer) {
+			for i, val := range t {
+				addArrayElem(w, strconv.FormatFloat(val, 'f', 10, 64), i != len(t)-1)
+			}
+		})
+	case []float32:
+		addElements(w, f.Key(), func(w *bytes.Buffer) {
+			for i, val := range t {
+				addArrayElem(w, strconv.FormatFloat(float64(val), 'f', 10, 32), i != len(t)-1)
+			}
+		})
+	}
+}
+
+func addElem(w *bytes.Buffer, k string, val string) {
+	w.WriteString(`"`)
+	w.WriteString(k)
+	w.WriteString(`":`)
+	w.WriteString(val)
+}
+
+func addElemQuoted(w *bytes.Buffer, k string, val string) {
+	w.WriteString(`"`)
+	w.WriteString(k)
+	w.WriteString(`":"`)
+	w.WriteString(val)
+	w.WriteString(`"`)
+}
+
+func addArrayElem(w *bytes.Buffer, val string, hasNext bool) {
+	w.WriteString(val)
+	if hasNext {
+		w.WriteString(`,`)
+	}
+}
+
+func addArrayElemQuoted(w *bytes.Buffer, val string, hasNext bool) {
+	w.WriteString(`"`)
+	w.WriteString(val)
+	w.WriteString(`"`)
+	if hasNext {
+		w.WriteString(`,`)
+	}
+}
+
+func addElements(w *bytes.Buffer, k string, fn func(w *bytes.Buffer)) {
+	w.WriteString(`"`)
+	w.WriteString(k)
+	w.WriteString(`":[`)
+	fn(w)
+	w.WriteString(`]`)
+}
---
+++ benchmarks/encoder/json_bench_test.go
@@ -0,0 +1,92 @@
+package encoder
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/damianopetrungaro/golog"
+)
+
+/**
+OUTCOME:
+
+goos: darwin
+goarch: amd64
+pkg: json_encoder_benchmarks
+cpu: Intel(R) Core(TM) i7-9750H CPU @ 2.60GHz
+BenchmarkJsonEncoder/encoding/json-12             526494              2233 ns/op            4998 B/op         70 allocs/op
+BenchmarkJsonEncoder/gojson-12                    783475              1624 ns/op            3427 B/op         27 allocs/op
+BenchmarkJsonEncoder/jsoniter-12                  783744              1667 ns/op            3428 B/op         27 allocs/op
+BenchmarkJsonEncoder/golog-12                    2231253               528.3 ns/op          1311 B/op          6 allocs/op
+PASS
+ok      json_encoder_benchmarks 6.689s
+
+*/
+
+var entry = golog.NewStdEntry(context.Background(), golog.INFO, "This is a log message", golog.Fields{
+	golog.Int("int", 10),
+	golog.Ints("ints", []int{1, 2, 3, 4, 5, 6, 7}),
+	golog.String("string", "a string"),
+	golog.Strings("strings", []string{"one", "one", "one", "one", "one", "one"}),
+	golog.Int("int_2", 10),
+	golog.Ints("ints_2", []int{1, 2, 3, 4, 5, 6, 7}),
+	golog.String("string_2", "a string"),
+	golog.Strings("strings_2", []string{"one", "one", "one", "one", "one", "one"}),
+	golog.Int("int_3", 10),
+	golog.Ints("ints_3", []int{1, 2, 3, 4, 5, 6, 7}),
+	golog.String("string_3", "a string"),
+	golog.Strings("strings_3", []string{"one", "one", "one", "one", "one", "one"}),
+
+	golog.Err(fmt.Errorf("an error occurred")),
+})
+
+func BenchmarkJsonEncoder(b *testing.B) {
+	b.Run("encoding/json", func(b *testing.B) {
+		b.RunParallel(func(pb *testing.PB) {
+			enc := &JsonEncoder{}
+			b.ResetTimer()
+			for pb.Next() {
+				if _, err := enc.StdLinEncode(entry); err != nil {
+					b.Fatal("encode failed")
+				}
+			}
+		})
+	})
+
+	b.Run("gojson", func(b *testing.B) {
+		b.RunParallel(func(pb *testing.PB) {
+			enc := &JsonEncoder{}
+			b.ResetTimer()
+			for pb.Next() {
+				if _, err := enc.GoJsonEncode(entry); err != nil {
+					b.Fatal("encode failed")
+				}
+			}
+		})
+	})
+
+	b.Run("jsoniter", func(b *testing.B) {
+		b.RunParallel(func(pb *testing.PB) {
+			enc := &JsonEncoder{}
+			b.ResetTimer()
+			for pb.Next() {
+				if _, err := enc.GoJsonEncode(entry); err != nil {
+					b.Fatal("encode failed")
+				}
+			}
+		})
+	})
+
+	b.Run("golog", func(b *testing.B) {
+		b.RunParallel(func(pb *testing.PB) {
+			enc := &JsonEncoder{}
+			b.ResetTimer()
+			for pb.Next() {
+				if _, err := enc.ManualEncode(entry); err != nil {
+					b.Fatal("encode failed")
+				}
+			}
+		})
+	})
+}
