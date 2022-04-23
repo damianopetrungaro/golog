dbcd2b40d78226535cd52487946044ecf9123af5
Damiano Petrungaro <damiano@pento.io>
2022-04-23 02:29:33 +0200

    feat: add tracing pkgs

---
+++ opencensus/opencensus.go
@@ -0,0 +1,30 @@
+package opencensus
+
+import (
+	"go.opencensus.io/trace"
+
+	"github.com/damianopetrungaro/golog"
+)
+
+var decorator golog.DecoratorFunc = func(e golog.Entry) golog.Entry {
+	span := trace.FromContext(e.Context()).SpanContext()
+
+	return e.With(golog.Fields{
+		TraceID(span),
+		SpanID(span),
+	})
+}
+
+func TraceDecorator() golog.Decorator {
+	return decorator
+}
+
+func TraceID(span trace.SpanContext) golog.Field {
+	const k = "trace_id"
+	return golog.String(k, span.TraceID.String())
+}
+
+func SpanID(span trace.SpanContext) golog.Field {
+	const k = "span_id"
+	return golog.String(k, span.SpanID.String())
+}
