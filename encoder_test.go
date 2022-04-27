b80b99c265d399b2def4eae27ec54e92c7b5e59c
Damiano Petrungaro <damiano@pento.io>
2022-04-27 22:13:22 +0200

    feat: add support for text encoder

--- encoder_test.go
+++ encoder_test.go
@@ -81,3 +81,56 @@ func TestJsonEncoder_Encode(t *testing.T) {
 		})
 	}
 }
+
+func TestTextEncoder_Encode(t *testing.T) {
+	cfg := DefaultTextConfig()
+
+	tests := map[string]struct {
+		entry   Entry
+		wantLog string
+	}{
+		"simple entry": {
+			entry:   NewStdEntry(context.Background(), DEBUG, "message", nil),
+			wantLog: fmt.Sprintln(`level="DEBUG" message="message"`),
+		},
+		"entry with string": {
+			entry:   NewStdEntry(context.Background(), INFO, "string message", Fields{String("name", "golog")}),
+			wantLog: fmt.Sprintln(`level="INFO" message="string message" name="golog"`),
+		},
+		"entry with int": {
+			entry:   NewStdEntry(context.Background(), WARN, "int message", Fields{Int("number", 101)}),
+			wantLog: fmt.Sprintln(`level="WARN" message="int message" number=101`),
+		},
+		"entry with string and int": {
+			entry:   NewStdEntry(context.Background(), ERROR, "string and int message", Fields{String("name", "golog"), Int("number", 101)}),
+			wantLog: fmt.Sprintln(`level="ERROR" message="string and int message" name="golog" number=101`),
+		},
+		"entry with array of booleans and array of floats": {
+			entry:   NewStdEntry(context.Background(), DEBUG, "array of booleans and array of floats", Fields{Bools("booleans", []bool{true, false, false, true}), Float64s("numbers", []float64{1, 1.34, 74.343})}),
+			wantLog: fmt.Sprintln(`level="DEBUG" message="array of booleans and array of floats" booleans=[true,false,false,true] numbers=[1.0000000000,1.3400000000,74.3430000000]`),
+		},
+	}
+
+	for name, test := range tests {
+		test := test
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			enc := NewTextEncoder(cfg)
+			w, err := enc.Encode(test.entry)
+			if err != nil {
+				t.Errorf("could not encode: %s", err)
+			}
+
+			buf := &bytes.Buffer{}
+			if _, err := w.WriteTo(buf); err != nil {
+				t.Errorf("could not write: %s", err)
+			}
+
+			if got := buf.String(); got != test.wantLog {
+				t.Error("could not match log")
+				t.Errorf("want: %s", test.wantLog)
+				t.Errorf("got: %s", got)
+			}
+		})
+	}
+}
--- go.mod
+++ go.mod
@@ -5,11 +5,11 @@ go 1.18
 require (
 	go.opencensus.io v0.23.0
 	go.opentelemetry.io/otel v1.6.3
+	go.opentelemetry.io/otel/trace v1.6.3
 )
 
 require (
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
-	go.opentelemetry.io/otel/trace v1.6.3 // indirect
 )
