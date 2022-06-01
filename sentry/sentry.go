c964f6c364fdd892e1cbf86f280899e5d3edaa3d
Antonio Pitasi <antonio@pitasi.dev>
2022-06-01 18:27:25 +0200

    feat(sentry): use Hub from context or fallback to the current one

--- sentry/sentry.go
+++ sentry/sentry.go
@@ -2,6 +2,7 @@ package sentry
 
 import (
 	"bytes"
+	"context"
 	"errors"
 	"fmt"
 
@@ -11,10 +12,23 @@ import (
 )
 
 type Writer struct {
-	Encoder                   golog.Encoder
-	Hub                       *sentry.Hub
-	ErrHandler                golog.ErrorHandler
+	Encoder    golog.Encoder
+	ErrHandler golog.ErrorHandler
+
+	// Hub is the sentry Hub used for capturing events. If nil, the
+	// sentry.CurrentHub() will be used.
+	//
+	// If the context of the golog Entry to be logged contains a sentry.Hub, it
+	// will be used instead.
+	Hub *sentry.Hub
+
+	// DefaultIsException set to true means that the when this Writer is used as
+	// a writer for the stdlib `log` package, its events will be treated as
+	// Sentry errors.
 	DefaultCaptureException bool
+
+	// CaptureExceptionFromLevel is the level at which an Entry will be
+	// treated as Sentry error instead of an arbitrary message.
 	CaptureExceptionFromLevel golog.Level
 }
 
@@ -31,21 +45,36 @@ func (w *Writer) WriteEntry(e golog.Entry) {
 		return
 	}
 
+	hub := w.getHub(e.Context())
 	if e.Level() >= w.CaptureExceptionFromLevel {
-		w.Hub.CaptureException(errors.New(buf.String()))
+		hub.CaptureException(errors.New(buf.String()))
 		return
 	}
 
-	w.Hub.CaptureMessage(buf.String())
-
+	hub.CaptureMessage(buf.String())
 }
 
 func (w *Writer) Write(msg []byte) (int, error) {
+	hub := w.getHub(nil)
+
 	if w.DefaultCaptureException {
-		w.Hub.CaptureException(errors.New(string(msg)))
+		hub.CaptureException(errors.New(string(msg)))
 		return len(msg), nil
 	}
 
-	w.Hub.CaptureMessage(string(msg))
+	hub.CaptureMessage(string(msg))
 	return len(msg), nil
+}
+
+func (w *Writer) getHub(ctx context.Context) *sentry.Hub {
+	ctxHub := sentry.GetHubFromContext(ctx)
+	if ctxHub != nil {
+		return ctxHub
+	}
+
+	if w.Hub != nil {
+		return w.Hub
+	}
+
+	return sentry.CurrentHub()
 }
