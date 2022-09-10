cbd0c72d364eb0af43b07a6967f67505dde21e8d
Damiano Petrungaro <damiano@pento.io>
2022-09-10 18:53:32 +0200

    feat(test): add a fake logger with matcher APIs

---
+++ test/matcher_test.go
@@ -0,0 +1,83 @@
+package test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/damianopetrungaro/golog"
+)
+
+func TestMatchEntry(t *testing.T) {
+	ctx := context.Background()
+	infoEntry := golog.NewStdEntry(ctx, golog.INFO, "Message one", golog.Fields{})
+	errorEntry := golog.NewStdEntry(ctx, golog.ERROR, "Message two", golog.Fields{})
+
+	tests := map[string]struct {
+		x, y   golog.Entry
+		errMsg string
+	}{
+		"entries are equal because nil": {
+			x: nil,
+			y: nil,
+		},
+		"entries are equal without fields": {
+			x: infoEntry,
+			y: infoEntry,
+		},
+		"entries are equal with fields": {
+			x: errorEntry.With(golog.String("key", "value")),
+			y: errorEntry.With(golog.String("key", "value")),
+		},
+		"entries are not equal because x is not a std entry": {
+			x:      &MockEntry{},
+			y:      infoEntry,
+			errMsg: "x is not a stdEntry",
+		},
+		"entries are not equal because y is not a std entry": {
+			x:      infoEntry,
+			y:      &MockEntry{},
+			errMsg: "y is not a stdEntry",
+		},
+		"entries are not equal because of different fields": {
+			x:      errorEntry.With(golog.String("key", "value")),
+			y:      errorEntry.With(golog.String("another_key", "value")),
+			errMsg: "could not match field value at index 0",
+		},
+		"entries are not equal because of different level": {
+			x:      golog.NewStdEntry(ctx, golog.INFO, "Message one", golog.Fields{}),
+			y:      golog.NewStdEntry(ctx, golog.DEBUG, "Message one", golog.Fields{}),
+			errMsg: "could not match level",
+		},
+		"entries are not equal because of different message": {
+			x:      golog.NewStdEntry(ctx, golog.INFO, "Message one", golog.Fields{}),
+			y:      golog.NewStdEntry(ctx, golog.INFO, "Message two", golog.Fields{}),
+			errMsg: "could not match message",
+		},
+		"entries are not equal because of different fields length": {
+			x:      errorEntry.With(golog.String("key", "value")),
+			y:      errorEntry,
+			errMsg: "could not match fields length",
+		},
+		"entries are not equal because of different context": {
+			x:      golog.NewStdEntry(context.Background(), golog.INFO, "Message one", golog.Fields{}),
+			y:      golog.NewStdEntry(context.TODO(), golog.INFO, "Message one", golog.Fields{}),
+			errMsg: "could not match context",
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			err := MatchEntry(test.x, test.y)
+			switch {
+			case test.errMsg != "":
+				if err.Error() != test.errMsg {
+					t.Fatalf("could not error message: %s", err)
+				}
+			default:
+				if err != nil {
+					t.Fatalf("could not match entry: %s", err)
+				}
+			}
+		})
+	}
+}
--- test/null.go
+++ test/null.go
@@ -1,27 +1,63 @@
 package test
 
 import (
-	"bufio"
-	"io"
+	"context"
+	"sync"
 
 	"github.com/damianopetrungaro/golog"
 )
 
+// NewNullLogger returns a null logger
+// useful for passing a logger as dependency
 func NewNullLogger() golog.Logger {
 	return newNullStdLogger()
 }
 
+// NewFakeLogger returns a logger
+// that writes entries to the passed inmemory writer
+// useful to check whether the logs are written as expected
+func NewFakeLogger(w *InMemWriter) golog.Logger {
+	return golog.New(w)
+}
+
+// NewNullCheckLogger returns a null check logger
+// useful for passing a logger as dependency
 func NewNullCheckLogger() golog.CheckLogger {
 	return newNullStdLogger()
 }
 
+// NewFakeCheckLogger returns a check logger
+// that writes entries to the passed inmemory writer
+// useful to check whether the logs are written as expected
+func NewFakeCheckLogger(w *InMemWriter) golog.Logger {
+	return golog.New(w)
+}
+
 func newNullStdLogger() golog.StdLogger {
-	return golog.New(
-		golog.NewBufWriter(
-			golog.NewJsonEncoder(golog.DefaultJsonConfig()),
-			bufio.NewWriter(io.Discard),
-			golog.DefaultErrorHandler(),
-			golog.DEBUG,
-		),
-	)
+	return golog.New(NewInMemWriter())
+}
+
+// InMemWriter is a Writer which keep in memory all the entries
+type InMemWriter struct {
+	Entries []golog.Entry
+	mu      sync.Mutex
+}
+
+// NewInMemWriter returns a InMemWriter
+func NewInMemWriter() *InMemWriter {
+	return &InMemWriter{}
+}
+
+// WriteEntry writes an Entry to the slice
+func (w *InMemWriter) WriteEntry(e golog.Entry) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+
+	w.Entries = append(w.Entries, e)
+}
+
+// Write writes an Entry to the slice
+func (w *InMemWriter) Write(msg []byte) (int, error) {
+	w.WriteEntry(golog.NewStdEntry(context.Background(), golog.DEBUG, string(msg), golog.Fields{}))
+	return len(msg), nil
 }
