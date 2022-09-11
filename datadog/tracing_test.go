3284599cc6c271e7de35e5774620906ec516988e
Damiano Petrungaro <damiano@pento.io>
2022-09-11 03:14:30 +0200

    test(datadog): increase unit test coverage

--- datadog/tracing_test.go
+++ datadog/tracing_test.go
@@ -5,23 +5,61 @@ import (
 )
 
 func TestTraceIDFromHexFormat(t *testing.T) {
-	traceID := "0ef42e66522e5a9183241106963acf99"
-	expectedTraceID := "9449696638118055833"
+	tests := map[string]struct {
+		traceID string
+		want    string
+	}{
+		"correct trace id": {
+			traceID: "0ef42e66522e5a9183241106963acf99",
+			want:    "9449696638118055833",
+		},
+		"too short trace id": {
+			traceID: "0123",
+			want:    "",
+		},
+		"invalid trace id": {
+			traceID: "xyzxyzxyzxyzxyzxyzxyzxyzxyzxyz",
+			want:    "",
+		},
+	}
 
-	result := TraceIDFromHexFormat(traceID)
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			result := TraceIDFromHexFormat(test.traceID)
 
-	if result != expectedTraceID {
-		t.Errorf("expected result trace id to be %q got %q", expectedTraceID, result)
+			if result != test.want {
+				t.Errorf("expected result trace id to be %q got %q", test.want, result)
+			}
+		})
 	}
 }
 
 func TestSpanIDFromHexFormat(t *testing.T) {
-	spanID := "0102040810203040"
-	expectedSpanID := "72624976668143680"
+	tests := map[string]struct {
+		spanID string
+		want   string
+	}{
+		"correct span id": {
+			spanID: "0102040810203040",
+			want:   "72624976668143680",
+		},
+		"too short span id": {
+			spanID: "0123",
+			want:   "",
+		},
+		"invalid span id": {
+			spanID: "xyzxyzxyzxyzxyzxyzxyzxyzxyzxyz",
+			want:   "",
+		},
+	}
 
-	result := SpanIDFromHexFormat(spanID)
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			result := SpanIDFromHexFormat(test.spanID)
 
-	if result != expectedSpanID {
-		t.Errorf("expected result span id to be %q got %q", expectedSpanID, result)
+			if result != test.want {
+				t.Errorf("expected result span id to be %q got %q", test.want, result)
+			}
+		})
 	}
 }
